ba957bb12c3019403dd10fb102487531e32ff3e5
Gopal Goel <mailgopalhere@gmail.com>
2016-03-19 01:32:14 +0530

    Commit

--- src/Test.java
+++ src/Test.java
@@ -3,7 +3,7 @@ import java.util.Date;
 
 class Account{
 	private String name;
-	private Integer balance;
+	private Double balance;
 	private Integer accountNumber;
 	private String password;
 	private String status;
@@ -12,7 +12,7 @@ class Account{
 	private static int nextAccountNumber=16000;
 	public static final double interest = 4.00;
 	
-	private Account(String name, int balance, String password){
+	private Account(String name, Double balance, String password){
 		this.name=name;
 		this.balance=balance;
 		accountNumber=nextAccountNumber++;
@@ -22,15 +22,55 @@ class Account{
 
 	}
 	
+	public void showAccountDetails(){
+		System.out.print("Name: ");
+		System.out.println(this.getName());
+		System.out.print("Account Number: ");
+		System.out.print(this.getAccountNumber());
+		System.out.print("Current Balance: ");
+		System.out.println(this.getAccountBalance());
+		System.out.print("Account Status: ");
+		System.out.println(this.getStatus());
+		showAllLoanDetails();
+	}
+
+	public void showAllLoanDetails(){
+		System.out.print("Total Loans Issued are: ");
+		System.out.println(loanList.length);
+		for(Loan loan : loanList){
+			if(loan.status=="active") loan.showLoanDetails();
+		}
+	}
+
+	public void payLoanDueAmount(Integer loanID){
+		for(Loan loan : loanList){
+			if ( loan.getLoanID() == loanID ){
+				if(loan.getDueAmount()<balance){
+					System.out.println("Sorry! Not enough balance in your account");
+				}
+				else {
+					balance-=loan.getDueAmount();
+					loan.status="inactive";
+				}
+			}
+		}
+	}
+
 	public void deposit(int amount){
+		double temp=balance;
 		balance+=amount;
+		if(temp<1000) {
+			System.out.println("Rs. 100 is deducted as your account balance was below \"Minimum Balance\" i.e Rs. 1000, before this transaction.");
+			balance-=100;
+		}
 		System.out.println("Your updated balance is: " + balance.toString());
 		if(balance>=0) status="active";
+		else status="frozen";
 	}
 	
 	public void withdraw(int amount){
 		if(amount<=balance+1000){
-			balance+=amount;
+			balance-=amount;
 			System.out.println("Your updated balance is: " + balance.toString());
 			if(balance<0) status="frozen";
 		}
@@ -39,12 +79,21 @@ class Account{
 		}
 	}
 	
+	public String getName(){
+		return name;
+	}
+	
+	public String getStatus(){
+		return status;
+	}
+	
+	
 	public int getAccountNumber(){
 		return accountNumber;
 	}
 	
-	public void showBalance(){
-		System.out.println("Your Balance is" + balance.toString());
+	public Double getAccountBalance(){
+		return balance;
 	}
 
 	public void issueNewLoan(String type,int amount,byte tenureInYears){
@@ -75,7 +124,7 @@ class Account{
 	public static Account openNewAccount() throws IOException{
 		BufferedReader br = new BufferedReader ( new InputStreamReader(System.in));
 		String name;
-		Integer amount;
+		Double amount;
 		String password;
 		do{
 			System.out.println("Lets open a new Account");
@@ -93,9 +142,9 @@ class Account{
 		// Input Amount
 		do{
 			try{
-				System.out.println("How much money would you like to deposit now? Minimum amount to open an account is Rs.10000");
-				amount = Integer.parseInt(br.readLine());
-				if(amount<10000) System.out.println("Sorry we cant open an amount with amount " + amount.toString());
+				System.out.println("How much money would you like to deposit now? Minimum amount to open an account is Rs.1000");
+				amount = Double.parseDouble(br.readLine());
+				if(amount<1000) System.out.println("Sorry we cant open an amount with amount " + amount.toString());
 				else {
 					System.out.println("Okay! You will have "+ amount.toString() + " as opening balance"); 
 					break;
@@ -139,6 +188,8 @@ abstract class Loan{
 	protected Date endDate;
 	protected byte tenureInYears;
 	public String status;
+	private int loanID;
+	private static int loanIssueNumber=1;
 	public Loan(int amount,byte tenureInYears){
 		this.amount=amount;
 		this.tenureInYears=tenureInYears;
@@ -146,10 +197,27 @@ abstract class Loan{
 		endDate = new Date();
 		endDate.setYear(startDate.getYear()+tenureInYears);
 		status="active";
+		loanID=loanIssueNumber++;
 	}
 	abstract public double getDueAmount();
 	abstract public void showLoanDetails();
 	abstract public double getLoanRate();
+
+	public double getLoanAmount(){
+		return amount;
+	}
+
+	public Date getStartDate(){
+		return startDate;
+	}
+
+	public Date getEndDate(){
+		return endDate;
+	}
+	
+	public int getLoanID(){
+		return loanID;
+	}
 }
 
 class EducationLoan extends Loan{
@@ -159,25 +227,34 @@ class EducationLoan extends Loan{
 
 	public EducationLoan(int amount, byte tenureInYears){
 		super(amount,tenureInYears);
+		amountDue=amount*tenureInYears*(100+rate)/100.0;
 	}
 	
-	public double getDueAmount(){
-		amountDue=amount*tenureInYears*(100+rate)/100.0;
-		return amountDue;
-	}
-
 	public void showLoanDetails(){
-		System.out.println(loanName);
-		System.out.print("Issue Date: ");
-		System.out.println(startDate);
+		System.out.print("Loan Name: ");
+		System.out.println(getLoanName());
+		System.out.print("Loan ID: ");
+		System.out.println(getLoanID());
+		System.out.print("Loan Amount: ");
+		System.out.println(getLoanAmount());
+		System.out.print("Loan Start Date: ");
+		System.out.println(getStartDate());
 		System.out.print("End Date: ");
-		System.out.println(endDate);
-		System.out.print("Due amount on End Date is: ");
+		System.out.println(getEndDate());
+		System.out.print("Due Amount: ");
 		System.out.println(getDueAmount());
 	}
 
 	public double getLoanRate(){
 		return rate;
+	}
+
+	public String getLoanName(){
+		return loanName;
+	}
+
+	public double getDueAmount(){
+		return amountDue;
 	}
 }
 
@@ -188,30 +265,56 @@ class HomeLoan extends Loan{
 	
 	public HomeLoan(int amount, byte tenureInYears){
 		super(amount,tenureInYears);
-	}
-	
-	public double getDueAmount(){
 		amountDue=amount*tenureInYears*(100+rate)/100;
-		return amountDue;
 	}
 
 	public void showLoanDetails(){
-		System.out.println(loanName);
-		System.out.print("Issue Date: ");
-		System.out.println(startDate);
+		System.out.print("Loan Name: ");
+		System.out.println(getLoanName());
+		System.out.print("Loan ID: ");
+		System.out.println(getLoanID());
+		System.out.print("Loan Amount: ");
+		System.out.println(getLoanAmount());
+		System.out.print("Loan Start Date: ");
+		System.out.println(getStartDate());
 		System.out.print("End Date: ");
-		System.out.println(endDate);
-		System.out.print("Due amount on End Date is: ");
+		System.out.println(getEndDate());
+		System.out.print("Due Amount: ");
 		System.out.println(getDueAmount());
 	}
 
 	public double getLoanRate(){
 		return rate;
 	}
+
+	public String getLoanName(){
+		return loanName;
+	}
+
+	public double getDueAmount(){
+		return amountDue;
+	}
 }
 
+class User{
+	public String name;
+	public String userName;
+	public String password;
+}
+
+class Admin extends User{
+
+}
+
+class Customer extends User{
+
+}
+
+class Bank{
 	
-public class Bank {
+}
+	
+public class Test {
 	public static void main (String[] args) throws IOException{
 		System.out.println("===============================");
 		System.out.println("Welcome to the Bank of Westeros");
